23461e3d83b4017c3a47d606ff0eeded941184ce
Alejandro Sanchez <aalejo@gmail.com>
2020-04-16 01:52:24 +0000

    removed solution

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ suites = ['♡', '♢', '♤', '♧']
 values = list(range(1,14))
 
 def get_random_card():
-    suite_position = random.randint(0,len(suites)-1)
-    value_position = random.randint(0,len(values)-1)
-    return suites[suite_position] + str(values[value_position])
+    # your code here
+    return "3♤"
     
 print(get_random_card())
